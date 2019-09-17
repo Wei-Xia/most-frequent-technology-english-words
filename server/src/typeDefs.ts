703c69d354bd980b0c26e357d556825f21d405e8
Wei Xia <xw900812@gmail.com>
2019-09-17 17:14:09 -0400

    Create typeDefs.ts

---
+++ server/src/typeDefs.ts
@@ -0,0 +1,15 @@
+import { gql } from 'apollo-server-express';
+
+export const typeDefs = gql`
+  type Query {
+    hello: String!
+    cats: [Cat!]!
+  }
+  type Cat {
+    id: ID!
+    name: String!
+  }
+  type Mutation {
+    createCat(name: String!): Cat!
+  }
+`;
